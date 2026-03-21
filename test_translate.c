1e5bfda519d84ed22d1877e381f03509ed7f31ed
Bernhard Teodor Thodesen <thodesen45@gmail.com>
2026-03-21 18:33:00 +0100

    Testfunksjon for Translate

--- test_translate.c
+++ test_translate.c
@@ -3,3 +3,61 @@
 #include <stdlib.h>
 #include <math.h>
 #include <assert.h>
+
+
+// A = (
+// 0 2 5 0
+// 0 0 0 4
+// 6 3 0 0
+// 0 0 1 7
+// )     
+
+
+
+void test_translate()
+{
+
+    double values[7] = {2, 5, 4, 6, 3, 1, 7};
+    int row[7] = {0, 0, 1, 2, 2, 3, 3};
+    int col[7] = {1, 2, 3, 0, 1, 2, 3};
+    int n = 4;
+    int nnz = 7;
+
+    struct sparse_mat_coo x;
+
+    x.val = values;
+    x.row_idx = row;
+    x.col_idx = col;
+    x.n = n;
+    x.nnz = nnz;
+
+    
+    struct sparse_mat_crs y;
+    
+    translate_coo_to_crs(&x, &y);
+
+    int actual_rowptr[5] ={0, 2, 3, 5, 7};
+
+    for(int i = 0;  i < 5; i++)
+    {
+        assert(y.row_ptr[i] == actual_rowptr[i]);
+    }
+
+    for(int i = 0; i < x.nnz; i ++)
+    {
+        assert(y.col_idx[i] == col[i]);
+        assert(abs(y.val[i]-values[i]) < 1e-7);
+
+    }
+
+    printf("Success\n");
+}
+
+
+
+
+int main()
+{   
+    test_translate();
+    return 0;
+}

f79a45972c6c81c6c60f57f438db9e8f7f9f1b58
Bernhard Teodor Thodesen <thodesen45@gmail.com>
2026-03-25 14:11:36 +0100

    Fjerne dobbelloop

--- translate_coo_to_crs.c
+++ translate_coo_to_crs.c
@@ -11,25 +11,15 @@ void translate_coo_to_crs(struct sparse_mat_coo *mat_coo, struct sparse_mat_crs 
     int nnz = mat_coo->nnz;
 
     int *row_ptr = malloc((n + 1) * sizeof(int));
-    int *count = malloc(n *sizeof(int));
-    int *next = malloc(n * sizeof(int));
-
-    
     row_ptr[0] = 0;
 
+    int *count = calloc(n,sizeof(int));
+    int *next = malloc(n * sizeof(int));
 
-    for(int i = 0; i <n; i++)
+    for(int i = 0; i < nnz; i++)
     {  
-        count[i] = 0;
-        for(int j = 0; j < nnz; j++)
-        {
-            if(i == mat_coo->row_idx[j])
-            {
-                count[i] += 1;
-            }
-        } 
+        count[mat_coo->row_idx[i]]++;     
     }
-
 
     for(int i = 1; i <= n; i++)
     {
@@ -49,14 +39,11 @@ void translate_coo_to_crs(struct sparse_mat_coo *mat_coo, struct sparse_mat_crs 
         next[x_i] ++;
     }
 
-
     mat_crs->n = n;
     mat_crs->nnz = nnz;
     mat_crs->col_idx = col2;
     mat_crs->val = val2;
     mat_crs->row_ptr = row_ptr;
-
-
 
 }
 

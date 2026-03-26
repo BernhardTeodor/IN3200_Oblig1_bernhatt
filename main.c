5302b5c9bff62707fe7b9b3f2e623b9b1d7e69b5
Bernhard Teodor Thodesen <thodesen45@gmail.com>
2026-03-26 15:42:23 +0100

    Implementert hele main

--- main.c
+++ main.c
@@ -10,22 +10,25 @@ int main (int nargs, char **args)
     double **A, **B;
     struct sparse_mat_coo S_coo, C_coo;
     struct sparse_mat_crs S_crs, C_crs;
+    
 
     // read from command line the name of a data file in the matrix market exchange format
     strcat(filename, "data/");
     strcat(filename, args[1]);
 
+
     // read a sparse matrix from the matrix market data file, and store it in S_coo
     read_sparse_matrix_from_file (filename, &S_coo);
+
 
     // create the corresponding COO data structure C_coo
     C_coo.n = S_coo.n; C_coo.nnz = S_coo.nnz;
     C_coo.row_idx = S_coo.row_idx;
     C_coo.col_idx = S_coo.col_idx;
     C_coo.val = (double*)malloc(C_coo.nnz * sizeof(double));
+
     
     // allocate A and B as 2D n x n arrays and assign suitable numerical values
-    // double **A, **B;
     int n = S_coo.n;
     A = malloc(n*sizeof(double *));
     A[0] = malloc(n*n*sizeof(double));
@@ -33,7 +36,6 @@ int main (int nargs, char **args)
     B = malloc(n*sizeof(double*));
     B[0] = malloc(n*n*sizeof(double)); 
 
-    // Fixing index
     for(int i = 0; i < n; i++)
     {
         A[i] = A[0] + i*n;
@@ -51,6 +53,7 @@ int main (int nargs, char **args)
         }
     }
 
+
     // // computation 1
     sampled_matrix_multiplication_coo (&C_coo, A, B, &S_coo);
 
@@ -63,18 +66,20 @@ int main (int nargs, char **args)
     
 
     // // translate from S_coo to S_crs
-    // translate_coo_to_crs (&S_coo, &S_crs);
+    translate_coo_to_crs (&S_coo, &S_crs);
+
 
     // // allocate CRS data structure C_crs
-    // C_crs.n = S_crs.n; C_crs.nnz = S_crs.nnz;
-    // C_crs.row_ptr = S_crs.row_ptr;
-    // C_crs.col_idx = S_crs.col_idx;
-    // C_crs.val = (double*)malloc(C_crs.nnz * sizeof(double));
-        
-    
+    C_crs.n = S_crs.n; C_crs.nnz = S_crs.nnz;
+    C_crs.row_ptr = S_crs.row_ptr;
+    C_crs.col_idx = S_crs.col_idx;
+    C_crs.val = (double*)malloc(C_crs.nnz * sizeof(double));
+
+
     // // computation 2
-    // sampled_matrix_multiplication_crs (&C_crs, A, B, &S_crs);
-    
+    sampled_matrix_multiplication_crs (&C_crs, A, B, &S_crs);
+
+
     // deallocate data structures
     free(S_coo.row_idx);
     free(S_coo.col_idx);
@@ -87,5 +92,7 @@ int main (int nargs, char **args)
     free(S_crs.row_ptr);
     free(S_crs.col_idx);
     free(S_crs.val);
+    free(C_crs.val);
+
     return 0;
 }
